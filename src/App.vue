153bbcfa4c56f37d46cd42fc1fea3ef746933b9f
the-man2004 <heinrichhb111@gmail.com>
2023-08-14 17:09:01 -0700

    fetching data based on user geolocation

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
     <h2>{{ temp }}°c</h2>
     <div></div>
   </div>
+
+  <h3>{{ weather }}</h3>
 </template>
 
 <script>
@@ -16,28 +18,42 @@ export default {
       fetching: true,
       city: "",
       temp: null,
+      weather: null,
+      latitude: null,
+      longitude: null,
     };
   },
   computed: {},
   methods: {
-    async fetchData(cityName) {
+    setPosition(position) {
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+
+      this.fetchData();
+    },
+    async fetchData() {
       this.fetching = true;
 
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=04400c94c3fa39734684083c69ac10aa`
-      );
-      const responseData = await response.json();
-      console.log(responseData);
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${this.latitude}&lon=${this.longitude}&cnt=5&appid=04400c94c3fa39734684083c69ac10aa&units=metric`
+        );
+        const responseData = await response.json();
+        console.log(responseData);
 
-      this.fetching = false;
+        this.fetching = false;
 
-      this.city = responseData.name;
-
-      this.temp = (responseData.main.temp - 273.15).toFixed();
+        this.city = responseData.city.name;
+        this.temp = responseData.list[0].main.temp.toFixed();
+        this.weather = responseData.list[0].weather[0].description;
+      } catch (err) {
+        console.error(err);
+        this.fetching = false;
+      }
     },
   },
   mounted() {
-    this.fetchData("london");
+    navigator.geolocation.getCurrentPosition(this.setPosition);
   },
 };
 </script>
